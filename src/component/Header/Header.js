3fbb898f3030a3875435f863f935453bae6372be
unknown <mdyasirofficial2001@gmail.com>
2022-09-29 10:36:57 +0600

    Heading added

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,9 +1,14 @@
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <div>
-      <h1>hello bangladesh</h1>
+      <h1 className="site-name">
+        <FontAwesomeIcon icon={faBook}></FontAwesomeIcon> Productivity-Club
+      </h1>
     </div>
   );
 };
